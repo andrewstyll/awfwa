a38b76c34c66423c1b6d25aca6d1064aee5bdb9c
andrewstyll <andrewstyll@gmail.com>
2016-06-13 13:10:58 -0400

    first revision of bubble sort O_o

---
+++ sort_and_search/bubble.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+class Bubble {
+    private:
+
+        swap(int *array, int a, int b) {
+            int tmp = a;
+            array[a] = b;
+            array[b] = tmp;
+        }
+
+    public:
+
+        bubbleSort(int *array, int n) {
+            for(int i = n-1; i > 0; i--) {
+                for(int j = 0; j < i; j++) {
+                    
+                }
+            }
+        }
+
+}
+
+int main() {
+    return 0;
+}
